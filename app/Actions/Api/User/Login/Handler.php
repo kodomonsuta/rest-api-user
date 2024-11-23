9081b2bfcd4f52b0ad6bf4ad92e3b181c9c80838
syaifudin latief <kodomonsuta@gmail.com>
2024-11-23 08:15:44 +0700

    fix dikit

--- app/Actions/Api/User/Login/Handler.php
+++ app/Actions/Api/User/Login/Handler.php
@@ -12,7 +12,6 @@ class Handler
             ValidateRequest::handle($request);
             AuthenticationUser::handle($request);
             SetBodyResponse::handle($request);
-
             return response()->api(200, 'success', $request->body_response, 'Client saved successfully');
         } catch (\Exception $e) {
             throw new ApiException($e->getMessage(), $e->getCode(), $e);
